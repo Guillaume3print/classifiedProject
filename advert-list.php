36fe75c35ffb9cb132105024ccf3d43954112b4d
Guillaume3print <guillaume@agence-sumo.com>
2020-04-17 15:11:04 +0200

    add select request to bdd to ads

--- advert-list.php
+++ advert-list.php
@@ -4,58 +4,24 @@ require_once(dirname(__FILE__) . "/components/navbar.php");
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
-        <ul class="list-group">
-            <li class="list-group-item">
-                <a href="advert-page.php">
-                    <h5>Annonce 1<span class="badge badge-secondary">New</span></h5>
-                </a>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis in perferendis impedit amet libero debitis accusantium labore repudiandae dignissimos aut totam reiciendis, ab, beatae voluptatibus ipsa qui blanditiis. Ex, architecto!Tenetur autem nisi quo minima, nesciunt, animi earum vitae eius explicabo eligendi fuga quisquam dignissimos ipsum velit sapiente voluptatem, repellat accusantium repellendus asperiores? Soluta commodi, sapiente delectus ratione deserunt nam!
-                </p>
-                <small>01/04/2020 à 22:36</small>
-            </li>
-        </ul>
+    <?php
+        require_once( dirname(__FILE__) . "/configs/database.php");
+        $req = $db->prepare("SELECT id, author_id, title, description, image_url, location, statut, DATE_FORMAT(created_at, 'Le %d/%m/%Y à %Hh%i') as created_at FROM ads");
+        $req->execute();
+        while( $result = $req->fetch(PDO::FETCH_ASSOC) ){
+        ?>
+
+            <div class="card" style="width: 18rem;">
+            <img src="<?= $result["image_url"]; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?= $result["title"]; ?></h5>
+                <p class="card-text"><?= $result["description"]; ?></p>
+                <span><?= $result["username"] ?> - <?= $result["created_at"]; ?></span>
+    
+                <a href="" class="btn btn-primary">Voir l'annonce</a>
+            </div>
         </div>
-        <div class="col-md-4">
-        <ul class="list-group">
-            <li class="list-group-item">
-                <a href="advert-page.php">
-                    <h5>Annonce 1<span class="badge badge-secondary">New</span></h5>
-                </a>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis in perferendis impedit amet libero debitis accusantium labore repudiandae dignissimos aut totam reiciendis, ab, beatae voluptatibus ipsa qui blanditiis. Ex, architecto!Tenetur autem nisi quo minima, nesciunt, animi earum vitae eius explicabo eligendi fuga quisquam dignissimos ipsum velit sapiente voluptatem, repellat accusantium repellendus asperiores? Soluta commodi, sapiente delectus ratione deserunt nam!
-                </p>
-                <small>01/04/2020 à 22:36</small>
-            </li>
-        </ul>
-        </div>
-        <div class="col-md-3">
-        <ul class="list-group">
-            <li class="list-group-item">
-                <a href="advert-page.php">
-                    <h5>Annonce 1<span class="badge badge-secondary">New</span></h5>
-                </a>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis in perferendis impedit amet libero debitis accusantium labore repudiandae dignissimos aut totam reiciendis, ab, beatae voluptatibus ipsa qui blanditiis. Ex, architecto!Tenetur autem nisi quo minima, nesciunt, animi earum vitae eius explicabo eligendi fuga quisquam dignissimos ipsum velit sapiente voluptatem, repellat accusantium repellendus asperiores? Soluta commodi, sapiente delectus ratione deserunt nam!
-                </p>
-                <small>01/04/2020 à 22:36</small>
-            </li>
-        </ul>
-        </div>
-        <div class="col-md-3">
-        <ul class="list-group">
-            <li class="list-group-item">
-                <a href="advert-page.php">
-                    <h5>Annonce 1<span class="badge badge-secondary">New</span></h5>
-                </a>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis in perferendis impedit amet libero debitis accusantium labore repudiandae dignissimos aut totam reiciendis, ab, beatae voluptatibus ipsa qui blanditiis. Ex, architecto!Tenetur autem nisi quo minima, nesciunt, animi earum vitae eius explicabo eligendi fuga quisquam dignissimos ipsum velit sapiente voluptatem, repellat accusantium repellendus asperiores? Soluta commodi, sapiente delectus ratione deserunt nam!
-                </p>
-                <small>01/04/2020 à 22:36</small>
-            </li>
-        </ul>
-        </div>
+        <?php } ?>
     </div>
 </div>
 <?php
